673cd8e998b7671299586eb99c7a76c7d918e0a1
Dylan Cole <logboarddjc@gmail.com>
2021-04-20 21:39:21 -0400

    add data from apis to elements and add class to hide elements on page load

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,75 +1,106 @@
-
-document.querySelector('input').addEventListener('change', (event) => {
-  
-  console.log(event.currentTarget.value) // movie
-})
-
+document.querySelector("input").addEventListener("change", (event) => {
+  // console.log(event.currentTarget.value) // movie
+});
 
 /* If the user clicks anywhere outside the select box,
 then close all select boxes: */
 // document.addEventListener("click", closeAllSelect);
 //End of the JS for the menu select button
-let youtubeKey = "AIzaSyB3LQ9556IHF2Cvci2B9S6FKyRVtMWlxa0"
-let playlistID = "PLopY4n17t8RDnEJnNXSwUbhvs4wNLpMe5"
-let youtubeAPI = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=" + playlistID + "&key=" + youtubeKey;
-let titlesArray = []
-let movies = []
+let youtubeKey = "AIzaSyB3LQ9556IHF2Cvci2B9S6FKyRVtMWlxa0";
+let playlistID = "PLopY4n17t8RDnEJnNXSwUbhvs4wNLpMe5";
+let youtubeAPI =
+  "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=" +
+  playlistID +
+  "&key=" +
+  youtubeKey;
+let titlesArray = [];
+let movies = [];
 
-document.querySelector('input').addEventListener('change', (event) => {
+document.querySelector("input").addEventListener("change", (event) => {
   // console.log(event.currentTarget.value) // movie
-})
+});
 
-fetch(youtubeAPI) 
-  .then(function(response) {
-    return response.json()
+fetch(youtubeAPI)
+  .then(function (response) {
+    return response.json();
   })
-  .then(function(data) {
+  .then(function (data) {
     for (let i = 0; i < 50; i++) {
-      let title = data.items[i].snippet.title
-      titlesArray.push(title)
-      let parts = title.replace("...", " ").split(" from ")
+      let title = data.items[i].snippet.title;
+      titlesArray.push(title);
+      let parts = title.replace("...", " ").split(" from ");
       if (parts.length > 1) {
-        let movie = parts[parts.length - 1]
+        let movie = parts[parts.length - 1];
         movies.push({
           movie,
           title,
           item: data.items[i],
           videoId: data.items[i].snippet.resourceId.videoId,
-        })
+        });
       }
     }
-    console.log(movies)
     for (const movie of movies) {
-      $('<option>')
-        .data('movie', movie)
+      $("<option>")
+        .data("movie", movie)
         .text(movie.movie)
-        .attr('value', movie.movie)
-        .appendTo('datalist')
+        .attr("value", movie.movie)
+        .appendTo("datalist");
     }
-  })
+  });
 
-$('input').on('change', (event) => {
-  let movieTitle = $(event.currentTarget).val()
-  getMovie(movieTitle)
-})
+$("input").on("change", (event) => {
+  let movieTitle = $(event.currentTarget).val();
+  getMovie(movieTitle);
+});
 
-let movieKey = "f420df924b19579fea697bc51f4a457d"
+let movieKey = "f420df924b19579fea697bc51f4a457d";
 function getMovie(movieTitle) {
-  let movie = movies.find((m) => m.movie === movieTitle)
-  let query = encodeURIComponent(movieTitle)
-  let movieAPI = "https://api.themoviedb.org/3/search/movie?api_key=" + movieKey + "&language=en-US&page=1&include_adult=false&query=" + query;
+  let movie = movies.find((m) => m.movie === movieTitle);
+  let query = encodeURIComponent(movieTitle);
+  let movieAPI =
+    "https://api.themoviedb.org/3/search/movie?api_key=" +
+    movieKey +
+    "&language=en-US&page=1&include_adult=false&query=" +
+    query;
   fetch(movieAPI)
     .then(function (response) {
       if (response.ok) {
-        return response.json()
+        return response.json();
       }
     })
     .then(function (data) {
-      console.log(data.results[0])
-    })
+      selectedMovie = data.results[0];
+      console.log(selectedMovie);
+
+      // title
+      console.log(selectedMovie.title);
+      $("#movie-title").text(selectedMovie.title);
+      // overview
+      console.log(selectedMovie.overview);
+      $("#movie-overview").text(selectedMovie.overview);
+      // release date
+      $("#release-date").text(selectedMovie.release_date);
+      // vote average
+      let movieRating = Math.floor(selectedMovie.vote_average / 2);
+      $("#movie-rating").empty();
+
+      function ratingSystem(movieRating) {
+        for (let i = 0; i < movieRating; i++) {
+          $("#movie-rating").append($("<i>").attr("class", "fas fa-star"));
+        }
+        let starRemainder = 5 - movieRating;
+        for (let i = 0; i < starRemainder; i++) {
+          $("#movie-rating").append($("<i>").attr("class", "far fa-star"));
+        }
+      }
+
+      ratingSystem(movieRating);
+      $('.hidden').removeClass("hidden");
+
+      // poster
+      $("#movie-poster").attr(
+        "src",
+        "https://image.tmdb.org/t/p/w500" + selectedMovie.poster_path
+      );
+    });
 }
-// release date
-// title
-// overview
-// vote average
-// poster
